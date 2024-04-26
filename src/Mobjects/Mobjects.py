04691cd136312accc6815107c738336ec1a775e6
AhmmedHesham <163453118+AhmmedHesham@users.noreply.github.com>
2024-04-27 00:21:19 +0300

    update DoubleArrow3D

--- src/Mobjects/Mobjects.py
+++ src/Mobjects/Mobjects.py
@@ -1,5 +1,4 @@
 from manim import *
-
 class DoubleArrow3D(Line3D):
     def __init__(
             self,
@@ -8,20 +7,22 @@ class DoubleArrow3D(Line3D):
             thickness: float = 0.02,
             height: float = 0.3,
             base_radius: float = 0.08,
+            
             color: ParsableManimColor = WHITE,
             **kwargs,
         ) -> None:
             super().__init__(
-                start=start, end=end, thickness=thickness, color=color, **kwargs
+                start=start, end=end, thickness=thickness, color=color, **kwargs,
             )
 
             self.length = np.linalg.norm(self.vect)
+      
             self.set_start_and_end_attrs(
-                self.start - height * -self.direction,
-                self.end - height * self.direction,
+                self.start - (height+0.1) * -self.direction,
+                self.end - (height+0.1) * self.direction,
                 **kwargs,
             )
-
+       
             self.cone = Cone(
                 direction=self.direction, base_radius=base_radius, height=height, **kwargs
             )
@@ -29,8 +30,8 @@ class DoubleArrow3D(Line3D):
                  direction=-self.direction, base_radius=base_radius, height=height, **kwargs
  
             )
-            self.cone.shift(end)
-            self.cone2.shift(start)
+            self.cone.shift(self.end + (height-0.2) * self.direction)
+            self.cone2.shift(self.start + (height-0.2) * -self.direction)
             self.add(self.cone, self.cone2)
             self.set_color(color)
     
